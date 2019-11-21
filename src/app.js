e0949ee0ec8bd6b924ce0ba5625a084758dd3c06
Ahmad AR <38329562+Ahmadar91@users.noreply.github.com>
2019-11-21 16:31:38 +0100

    nodemon with babel

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+import request from 'request'
+import cheerio from 'cheerio'
+// const request = require('request')
+// const cheerio = require('cheerio')
 console.log('srape')
+
+request('http://vhost3.lnu.se:20080/weekend', function (error, response, body) {
+  console.log('error:', error) // Print the error if one occurred
+
+  const page = cheerio.load(body)
+  const ol = page('a')
+  ol.each(function (index, element) {
+    console.log(page(this).text())
+    if (page(this).text() === 'Calendar') {
+      console.log(element.attribs.href)
+    }
+  })
+})
