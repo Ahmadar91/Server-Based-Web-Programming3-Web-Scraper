5f6deba9b94a768fb88412266200ec328e032e0f
Ahmad AR <38329562+Ahmadar91@users.noreply.github.com>
2019-11-21 21:05:29 +0100

    fixes

--- src/scrape.js
+++ src/scrape.js
@@ -8,6 +8,7 @@ import cheerio from 'cheerio'
 export function extractLinks (urls) {
   const url = urls + ''
   console.log(url)
+  const items = []
   request(url, function (error, response, body) {
     console.log('error:', error) // Print the error if one occurred
     const $ = cheerio.load(body)
@@ -15,8 +16,20 @@ export function extractLinks (urls) {
     ol.each(function (index, element) {
       console.log($(this).text())
       if ($(this).text() === 'Calendar') {
+        items.push(element.attribs.href)
+        getCalender(items[0])
         console.log(element.attribs.href)
       }
     })
+  })
+}
+
+function getCalender (urls) {
+  const url = urls + ''
+  console.log(url)
+  request(url, function (error, response, body) {
+    console.log('error:', error) // Print the error if one occurred
+    const $ = cheerio.load(body)
+    console.log($('ul').html())
   })
 }
